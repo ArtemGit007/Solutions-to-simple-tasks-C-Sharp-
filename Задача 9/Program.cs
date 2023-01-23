93255a5ec8cc3ebab6889538a801fe730cdeb65a
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-23 14:33:40 +0300

    Added ex.9

---
+++ Задача 9/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Задача 9. Составить программу обмена значениями двух переменных величин.
+
+Console.WriteLine("Введите переменную А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную В:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int buffer = a;
+
+a = b;
+b = buffer;
+
+Console.WriteLine(" Новое значение перенменной A {0}", a);
+Console.WriteLine(" Новое значение перенменной В {0}", b);
