b17557f8213e5e9452cc741948adea563d694ae1
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-23 14:55:50 +0300

    Added ex.11

---
+++ Задача 11/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задача 11. 1. Составить программу обмена значениями трех переменных величин а, b, c по следующей схеме: 
+// b присвоить значение c, а присвоить значение b, с присвоить значение а.
+
+Console.WriteLine("Введите переменную а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную в: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную с: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int temp = c;
+
+c = a;
+a = b;
+b = temp;
+
+Console.WriteLine("Новое значение переменной а = {0}", a);
+Console.WriteLine("Новое значение переменной b = {0}", b);
+Console.WriteLine("Новое значение переменной c = {0}", c);
