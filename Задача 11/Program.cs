263fbb448a47c686517902ecf1b86d94df614762
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-24 20:34:42 +0300

    Added ex. 12

--- Задача 11/Program.cs
+++ Задача 11/Program.cs
@@ -1,19 +1,13 @@
-﻿//  Задача 11. 1. Составить программу обмена значениями трех переменных величин а, b, c по следующей схеме: 
-// b присвоить значение c, а присвоить значение b, с присвоить значение а.
-
-Console.WriteLine("Введите переменную а: ");
+﻿ 
+Console.WriteLine(" Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите переменную в: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите переменную с: ");
-int c = Convert.ToInt32(Console.ReadLine());
-
-int temp = c;
-
-c = a;
-a = b;
-b = temp;
-
-Console.WriteLine("Новое значение переменной а = {0}", a);
-Console.WriteLine("Новое значение переменной b = {0}", b);
-Console.WriteLine("Новое значение переменной c = {0}", c);
+double del = 0.0;
+if(a>=100 &&a < 1000)
+{
+    del = a%10000.0;
+    Console.WriteLine(" Последняя цифра: " + del);
+}
+else
+{
+Console.WriteLine(" Введено не трёхзначное число");
+}
