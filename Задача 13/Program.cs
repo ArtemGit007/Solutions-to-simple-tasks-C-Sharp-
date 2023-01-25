bf59557f9f1f9d8d64577cb2d43d67bda6a7aa7d
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-25 18:50:29 +0300

    Added ex.13

---
+++ Задача 13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Найти произведение цифр заданного четырехзначного числа.
+
+Console.WriteLine(" Введите четырехзначное число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+
+//string _str = i.ToString();
+
+//for (int j = _str.Length; j=>0; j--)
+//   Console.WriteLine(_str[j]);
+
+int c = i;
+string str = c.ToString();
+int[] b = new int[str.Length];
+for (int j = 0; j < str.Length; j++)
+{
+    b[j] = int.Parse(str[j].ToString());
+
+}
+
+int res = b[0] * b[1] * b[2] * b[5];
+
+Console.WriteLine(res);
+
+
+
