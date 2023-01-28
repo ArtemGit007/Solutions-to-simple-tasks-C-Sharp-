87477c7ae1010981c40f9ace4b67d8f5330d361a
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-28 19:45:22 +0300

    Added ex.17

---
+++ Задача 17/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Известны координаты на плоскости двух точек. Составить программу вычисления расстояния между ними.
+
+Random rnd = new Random();
+
+int x1 = rnd.Next(-100, 100);
+Console.WriteLine("Значение х1 равно {0}", x1);
+int y1 = rnd.Next(-100, 100);
+Console.WriteLine("Значение y1 равно {0}", y1);
+int x2 = rnd.Next(-100, 100);
+int y2 = rnd.Next(-100, 100);
+Console.WriteLine(" Координаты второй точки {0}, {1}", x2, y2);
+double d = Math.Sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1)));
+
+Console.WriteLine(" Расстояние между точками a и в равно {0}", d);
