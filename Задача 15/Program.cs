be6e91e1ef8f661ddb6cb14f09d1f388dac1a742
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-28 19:43:51 +0300

    Added ex.15

---
+++ Задача 15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вычислить периметр и площадь прямоугольного треугольника по заданным длинам двух катетов а и b.
+ 
+Console.WriteLine(" Введите длину катета а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите длину катета b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double sque = (a * b)/2;
+Console.WriteLine(" Площадь треугольника равнa {0}", sque);
+
+double per = Math.Sqrt((a * a) + (b * b)) + a + b;
+Console.WriteLine(" Периметр треугольника равен {0}", per);
