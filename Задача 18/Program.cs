71a2693c8dc41d803571a945cfbb29fbad5f443b
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-28 21:00:17 +0300

    Added ex.18

---
+++ Задача 18/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дан радиус окружности. Найти длину окружности и площадь круга.
+
+Console.WriteLine(" Введите радиус окружности:");
+int r = Convert.ToInt32(Console.ReadLine());
+
+double pi = 3.14;
+double c = 2 * pi * r;
+
+Console.WriteLine(" Длина окружности равна {0}", c);
+
+double sque = pi*r*r;
+Console.WriteLine(" Площадь круга равна {0}", sque);
+
