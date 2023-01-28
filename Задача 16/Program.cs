1f597a3e7565de44227fb924084c35efb2ea6d1a
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-28 19:44:41 +0300

    Added ex. 16

---
+++ Задача 16/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вычислить расстояние между двумя точками с данными координатами (х1, у1) и (х2, у2).
+
+Console.WriteLine(" Введите координату х1:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите координату y1:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите координату х2:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите координату y2:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1)));
+
+Console.WriteLine(" Расстояние между точками a и в равно {0}", d);
