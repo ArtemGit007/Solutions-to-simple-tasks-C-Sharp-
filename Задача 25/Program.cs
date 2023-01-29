c2c0a1f64ffc17f857b88e863d95373f8b53161d
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-29 23:46:38 +0300

    Added ex.25

---
+++ Задача 25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вычислить периметр и площадь прямоугольника по заданным пользователем 
+// длинам сторон.
+
+Console.WriteLine("Введите значение стороны а:"); 
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение стороны b:"); 
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+double per = (a + b)*2;
+Console.WriteLine("Периметр прямоугольника равен {0}", per);
+
+double sque = a * b;
+Console.WriteLine("Площадь  прямоугольника равна {0}", sque);
