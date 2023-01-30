c9fb136777cec37c9cf87c0c788428d5b05fbbdb
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-31 01:16:56 +0300

    Added ex.36

---
+++ Задача 36/Program.cs
@@ -0,0 +1,10 @@
+﻿// Составить программу вычисления значения функции 
+// x=12a^2+7a-16 при любом значении а.
+
+string buf;
+Console.WriteLine(" Введите вещественное число a: ");
+buf = Console.ReadLine();
+double a = Convert.ToDouble(buf);
+
+double x = (12*a*a*2) + 7*a - 16;
+Console.WriteLine(x);
