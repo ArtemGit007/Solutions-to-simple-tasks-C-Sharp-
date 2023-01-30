a7abf15d334ec53e68db924e1640c0142abc0075
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-31 00:54:05 +0300

    Added ex.34

---
+++ Задача 34/Program.cs
@@ -0,0 +1,20 @@
+﻿ // Дано вещественное число а. Пользуясь только операцией умножения, 
+// получить: а^21 за шесть операций.
+
+string buf;
+Console.WriteLine(" Введите вещественное число a: ");
+buf = Console.ReadLine();
+double a = Convert.ToDouble(buf);
+
+double i = a * a; // i = a^2
+double b = i * i; // b = a^4
+double c = b * b; // c = a^8
+double d = c * c; // d = a^16
+double e = d * a; // c = a^17
+double x = e * b; // x = a^21
+
+Console.WriteLine(" a в 21-ой степени = {0} ",  x);
+
+//  Проверка методом
+double q = Math.Pow(a, 21);
+Console.WriteLine(q);
