124c25c1938adf02a8a2b372da942f18748f887f
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-31 00:49:18 +0300

    Added ex. 33

---
+++ Задача 33/Program.cs
@@ -0,0 +1,20 @@
+﻿ // Дано вещественное число а. Пользуясь только операцией умножения, 
+// получить: а^64 за шесть операций.
+
+ string buf;
+ Console.WriteLine(" Введите вещественное число а: ");
+ buf = Console.ReadLine();
+ double a = double.Parse(buf);
+
+double i = a * a; // i = a^2
+double b = i * i; // b = a^4
+double c = b * b; // c = a^8
+double d = c * c; // d = a^16
+double e = d * d; // c = a^32
+double x = e * e; // x = a^64
+
+Console.WriteLine(" a в 64-ой степени = {0} ",  x);
+
+//  Проверка методом
+double q = Math.Pow(a, 64);
+Console.WriteLine(q);
