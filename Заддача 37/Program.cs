5656c30f3e71fae0c33f37afcf16cb62debfeb42
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-01 00:24:17 +0300

    Added ex.37

---
+++ Заддача 37/Program.cs
@@ -0,0 +1,10 @@
+﻿// Составить программу вычисления значения функции y=7x^2+3x-6 
+// при любом значении x.
+
+string buf;
+Console.WriteLine("Введите любое число х:");
+buf = Console.ReadLine();
+double x = Convert.ToDouble(buf);
+
+double y = 7*x*x*2 + 3*x - 6;
+Console.WriteLine(y);
